7ac5769eea78b73e5bac54fb8dd9416af47128c0
ikuma-t <tadokorodev@gmail.com>
2023-02-26 11:38:01 +0900

    [from now] 2023/02/26 11:38:01
    diff --git a/security-handson/routes/api.js b/security-handson/routes/api.js
    index 200052f..dd08b83 100644
    --- a/security-handson/routes/api.js
    +++ b/security-handson/routes/api.js
    @@ -1,12 +1,29 @@
     const express = require("express");
     const router = express.Router();
    
    +const allowList = [
    +  // Q. これって、http://127.0.0.1:4949だとどうなる？
    +  // A. Origin違いとみなされて、CORSエラーになる
    +  "http://localhost:4949",
    +  "http://site.example:4949",
    +];
    +
    +router.use((req, res, next) => {
    +  if (req.headers.origin && allowList.includes(req.headers.origin)) {
    +    res.header("Access-Control-Allow-Origin", req.headers.origin);
    +  }
    +  if (req.method === "OPTIONS") {
    +    res.header("Access-Control-Allow-Headers", "X-Token");
    +  }
    +  next();
    +});
    +
     router.get("/", (req, res) => {
       res.setHeader("X-Timestamp", Date.now());
       let message = req.query.message;
       const lang = req.headers["X-Lang"];
    
    -  if (!message) {
    +  if (message === "") {
         res.status(400);
         if (lang === "ja") {
           message = "クエリ文字列が空です";
    
    [from now] 2023/02/26 11:38:11
    
    diff --git a/security-handson/public/attacker.html b/security-handson/public/attacker.html
    new file mode 100644
    index 0000000..93e4a53
    --- /dev/null
    +++ b/security-handson/public/attacker.html
    @@ -0,0 +1,19 @@
    +<!DOCTYPE html>
    +<html lang="en">
    +<head>
    +    <meta charset="UTF-8">
    +    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    +    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    +    <title>attacker.example</title>
    +    <script>
    +        function load() {
    +            const userInfo = frm.document.querySelector('#user_info');
    +            console.log(userInfo.textContent);
    +        }
    +    </script>
    +</head>
    +<body>
    +    <div>ここに罠コンテンツ</div>
    +    <iframe name="frm" onload="load()" src="http://site.example:4949/user.html" width="80%" />
    +</body>
    +</html>
    \ No newline at end of file
    diff --git a/security-handson/public/user.html b/security-handson/public/user.html
    new file mode 100644
    index 0000000..16a6470
    --- /dev/null
    +++ b/security-handson/public/user.html
    @@ -0,0 +1,16 @@
    +<!DOCTYPE html>
    +<html lang="en">
    +<head>
    +    <meta charset="UTF-8">
    +    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    +    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    +    <title>ログインユーザー情報</title>
    +</head>
    +<body>
    +   <ul id="user_info">
    +    <li>ログインID: frontend_security</li>
    +    <li>メールアドレス: frontend_security@mail.example</li>
    +    <li>住所： 東京都〇〇1-2-2</li>
    +   </ul>
    +</body>
    +</html>
    \ No newline at end of file

--- security-handson/routes/api.js
+++ security-handson/routes/api.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+const allowList = [
+  // Q. これって、http://127.0.0.1:4949だとどうなる？
+  // A. Origin違いとみなされて、CORSエラーになる
+  "http://localhost:4949",
+  "http://site.example:4949",
+];
+
+router.use((req, res, next) => {
+  if (req.headers.origin && allowList.includes(req.headers.origin)) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+  }
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", "X-Token");
+  }
+  next();
+});
+
 router.get("/", (req, res) => {
   res.setHeader("X-Timestamp", Date.now());
   let message = req.query.message;
   const lang = req.headers["X-Lang"];
 
-  if (!message) {
+  if (message === "") {
     res.status(400);
     if (lang === "ja") {
       message = "クエリ文字列が空です";
