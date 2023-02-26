86b601983896cdbc6830c1ad9ebfda298967c0aa
ikuma-t <tadokorodev@gmail.com>
2023-02-26 09:34:36 +0900

    [from now] 2023/02/26 09:34:36
    diff --git a/security-handson/server.js b/security-handson/server.js
    index f6aa8d4..2240935 100644
    --- a/security-handson/server.js
    +++ b/security-handson/server.js
    @@ -1,7 +1,10 @@
     const express = require("express");
    +const api = require("./routes/api");
    +
     const app = express();
     const port = 4949;
    
    +app.use("/api", api);
     app.use(express.static("public"));
    
     app.get("/", (req, res) => {

--- security-handson/server.js
+++ security-handson/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const api = require("./routes/api");
+
 const app = express();
 const port = 4949;
 
+app.use("/api", api);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
