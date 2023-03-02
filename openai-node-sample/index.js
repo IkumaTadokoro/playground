d181f1262bcb3e6d529896cee7f61504ce6ce61c
ikuma-t <tadokorodev@gmail.com>
2023-03-02 09:18:44 +0900

    [from now] 2023/03/02 09:18:44
    diff --git a/openai-node-sample/index.js b/openai-node-sample/index.js
    index 1ba38be..2eedecf 100644
    --- a/openai-node-sample/index.js
    +++ b/openai-node-sample/index.js
    @@ -1,18 +1,18 @@
     const { Configuration, OpenAIApi } = require("openai");
     require("dotenv").config();
    
    -console.log(process.env.OPENAI_API_KEY);
     const configuration = new Configuration({
       apiKey: process.env.OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
    
    +const content = `やっべ、ChatGPT API試してたらもう9時過ぎてた！一応9時が始業時刻なんだけど、どうしたらいいかな？`;
    +console.log(content);
    +
     (async () => {
    -  const response = await openai.createCompletion({
    -    model: "text-davinci-003",
    -    prompt: "Chat GPTについて教えて。",
    -    temperature: 0,
    -    max_tokens: 100,
    +  const response = await openai.createChatCompletion({
    +    model: "gpt-3.5-turbo",
    +    messages: [{ role: "user", content }],
       });
    -  console.log(response.data.choices[0].text);
    +  console.log(response.data.choices[0].message.content);
     })();

--- openai-node-sample/index.js
+++ openai-node-sample/index.js
@@ -1,18 +1,18 @@
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 
-console.log(process.env.OPENAI_API_KEY);
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+const content = `やっべ、ChatGPT API試してたらもう9時過ぎてた！一応9時が始業時刻なんだけど、どうしたらいいかな？`;
+console.log(content);
+
 (async () => {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Chat GPTについて教えて。",
-    temperature: 0,
-    max_tokens: 100,
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content }],
   });
-  console.log(response.data.choices[0].text);
+  console.log(response.data.choices[0].message.content);
 })();
